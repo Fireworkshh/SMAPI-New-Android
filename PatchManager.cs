9a59ed7e911975f22bcf686ad1796fa7aef7607d
Administrator <Administrator@FQ-MZKCNCCTCEVP>
2024-12-07 12:47:59 +0800

    .

--- PatchManager.cs
+++ PatchManager.cs
@@ -14,7 +14,7 @@ namespace StardewModdingAPI
         {
             try
             {
-                ApplyTypeDefinitionPatches();
+                //ApplyTypeDefinitionPatches();
                 
                 ApplyPatch();
                 ApplyResetLocalStatePatch();
